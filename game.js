7463aa052b7ef4d3bba5c509f4190fdee10bea06
Siddhartha <siddharthac@iitbhilai.ac.in>
2020-11-05 19:44:52 +0530

    Jquery Update 3 Code Refactoring

---
+++ game.js
@@ -0,0 +1,55 @@
+//initialize
+var colors = ["green", "red", "yellow", "blue"];
+var levelSequence = [];
+var currentSequence = [];
+var level = 0;
+
+// start the game
+$("*").keydown(function () {
+	if (level == 0) {
+		levelUp();
+	}
+});
+
+//Playing the game
+$(".btn").click(function () {
+	if (level != 0 && currentSequence.length != 0) {
+		if (this.id != currentSequence[0]) {
+			soundFx("sounds/wrong.mp3");
+			currentSequence = [];
+			levelSequence = [];
+			level = 0;
+			$("#level-title").text("Press Any Key to restart");
+		} else {
+			soundFx("sounds/" + currentSequence[0] + ".mp3");
+			currentSequence.shift();
+			if (currentSequence.length == 0) {
+				levelUp();
+			}
+		}
+	}
+});
+
+//Add a color to sequence after every level
+function addSequence() {
+	var nextSequence = colors[Math.floor(Math.random() * 4)];
+	soundFx("sounds/" + nextSequence + ".mp3");
+	levelSequence.push(nextSequence);
+	$("#" + nextSequence).addClass("pressed");
+	setTimeout(function () {
+		$("#" + nextSequence).removeClass("pressed");
+	}, 200);
+}
+
+//level up
+function levelUp() {
+	$("#level-title").text("Level " + ++level);
+	addSequence();
+	currentSequence = Array.from(levelSequence);
+}
+
+//sound effect
+function soundFx(soundPath) {
+	var sound = new Audio(soundPath);
+	sound.play();
+}
